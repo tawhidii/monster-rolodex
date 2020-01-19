1c8c2d03dff3dcc5f2c55486994b889b6c13f09b
Khademul Bari <tawhidi.pro@gmail.com>
2020-01-19 22:48:34 +0600

    Updates

---
+++ precache-manifest.c405493a8d822e56e1e31bdf51b29e74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1bc33c9692f3e487e2de6ff2151badb",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "6a82c2ff071b61e625fd",
+    "url": "/monster-rolodex/static/css/main.1e4b8172.chunk.css"
+  },
+  {
+    "revision": "c728ba4a2366e885ec54",
+    "url": "/monster-rolodex/static/js/2.d8d20882.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.d8d20882.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6a82c2ff071b61e625fd",
+    "url": "/monster-rolodex/static/js/main.98bda9ef.chunk.js"
+  },
+  {
+    "revision": "cd391451e66ac7af83df",
+    "url": "/monster-rolodex/static/js/runtime-main.1356d2e6.js"
+  }
+]);
